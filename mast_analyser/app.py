e4061a979be3c35127af01fc1f9d5dcb9c62443c
James <ball.j.work@gmail.com>
2022-02-09 13:07:46 +0000

    readme updated

--- mast_analyser/app.py
+++ mast_analyser/app.py
@@ -1,7 +1,7 @@
 from app.app import App
 
 # originally I wanted to run this file as module i.e. python -m mast_analyser
-# when in the top level directory
+# when in the top level directory, but not working locally.
 
 mast_analyser = App()
 
